b0c0de2c6422998372148be8759a7d42fd194485
Cyrus <cdbalsara@icloud.com>
2016-10-03 16:36:38 +0100

    created car class,included speed and fuel levels and exposed those levels.

---
+++ car_lab.rb
@@ -0,0 +1,20 @@
+class Car
+
+  attr_reader(:fuel_level, :speed)
+
+    def initialize(fuel_level, speed)
+      @fuel_level = fuel_level
+      @speed = speed
+    end 
+
+
+
+
+
+
+
+
+
+
+
+end 
---
+++ specs/car_lab_specs.rb
@@ -0,0 +1,20 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../car_lab')
+
+class TestCar < MiniTest::Test
+
+
+def test_name
+  car_called_speedy = Car.new(100, 0)
+end 
+
+
+
+
+
+
+
+
+
+end
