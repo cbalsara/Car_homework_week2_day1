514b0673818dd35f8da3eb09d39d0570e9182ad3
Cyrus Balsara <cyrusbalsara@cyruss-mbp.home>
2017-02-11 11:56:18 +0000

    created engine class and tested that engine speed starts at 0

---
+++ engine.rb
@@ -0,0 +1,13 @@
+class Engine
+attr_reader :speed, :fuel
+  def initialize(engine_speed, engine_fuel)
+    @speed = engine_speed
+    @fuel = engine_fuel
+  end 
+
+  def accelerate
+    @fuel = @fuel - 50 
+    @speed = @speed + 100
+  end 
+
+end 
---
+++ specs/engine_spec.rb
@@ -0,0 +1,20 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../engine')
+
+class TestEngine < MiniTest::Test
+
+  def setup
+    @sports_engine = Engine.new(0, 100)
+  end 
+
+  def test_engine_speed 
+    assert_equal(0, @sports_engine.speed)
+  end 
+
+
+
+
+
+
+end 
